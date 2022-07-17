2ebab254df159f2427608d134c09c687d89c4e15
aaaicu <aaaaicu@gmail.com>
2022-07-18 01:09:40 +0900

    bugfix: 호스트 설정 수정

--- src/component/oauth/OAuthUrl.js
+++ src/component/oauth/OAuthUrl.js
@@ -1,11 +1,11 @@
 const CLIENT_ID = "e82fa866da628313f8a8e42774b72077";
 //LOCAL
-export const FRONT_HOST = "http://localhost:3000";
-export const API_HOST = "http://localhost:9099";
+// export const FRONT_HOST = "http://localhost:3000";
+// export const API_HOST = "http://localhost:9099";
 
 //PROD
-// export const FRONT_HOST = "https://www.yummy-things.xyz";
-// export const API_HOST = "https://api.yummy-things.xyz";
+export const FRONT_HOST = "https://www.yummy-things.xyz";
+export const API_HOST = "https://api.yummy-things.xyz";
 
 const REDIRECT_URI =  `${FRONT_HOST}/callback/kakao`;
 
