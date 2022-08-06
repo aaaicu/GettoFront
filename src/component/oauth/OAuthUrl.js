a3bb42db2829811118c9b805e400d9a9fb0acf46
aaaicu <aaaaicu@gmail.com>
2022-08-07 03:31:06 +0900

    feat: 빌드환경에 따라 url변경되도록 설정

--- src/component/oauth/OAuthUrl.js
+++ src/component/oauth/OAuthUrl.js
@@ -1,11 +1,15 @@
 const CLIENT_ID = "e82fa866da628313f8a8e42774b72077";
 //LOCAL
-// export const FRONT_HOST = "http://localhost:3000";
-// export const API_HOST = "http://localhost:9099";
+export const DEV_FRONT_HOST = "http://localhost:3000";
+export const DEV_API_HOST = "http://localhost:9099";
 
 //PROD
-export const FRONT_HOST = "https://www.yummy-things.xyz";
-export const API_HOST = "https://api.yummy-things.xyz";
+export const PRD_FRONT_HOST = "https://www.yummy-things.xyz";
+export const PRD_API_HOST = "https://api.yummy-things.xyz";
+
+
+export const FRONT_HOST = process.env.NODE_ENV === 'production'? `${PRD_FRONT_HOST}` :`${DEV_FRONT_HOST}`;
+export const API_HOST = process.env.NODE_ENV === 'production'? `${PRD_API_HOST}` :`${DEV_API_HOST}`;
 
 const REDIRECT_URI =  `${FRONT_HOST}/callback/kakao`;
 
