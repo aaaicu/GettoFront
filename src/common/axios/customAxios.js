0dbb59947c950bfd406ee4822b943716124bc132
aaaicu <aaaaicu@gmail.com>
2022-08-17 23:28:46 +0900

    bugfix: 토큰 만료시 무한 재발급요청 → 재발급 받은 토큰을 재요청 client config에 추가하지않아서 발생한 문제

--- src/common/axios/customAxios.js
+++ src/common/axios/customAxios.js
@@ -18,6 +18,7 @@ api.interceptors.response.use(
   if(error.response.status === 403 || error.response.status === 401 ){
     const result = retryReissueAccessToken(localStorage.getItem('refreshToken'), error);
 
+    // 재요청 후에도 실패하면 로그아웃 처리
     result.catch(function(error) {
       console.log(error);
       window.location.href = '/logout';
@@ -30,15 +31,22 @@ api.interceptors.response.use(
 const retryReissueAccessToken = async function(refreshToken, error){ 
   console.log('=========== retry ============');
   const res = await defaultApi.get('/auth/reissue?refreshToken='+ refreshToken);
-  await resetToken(res.data.refreshToken, res.data.accessToken);
-  const data = await api.request(error.config);
+  const config = await resetToken(res.data.refreshToken, res.data.accessToken, error.config);
+  
+  // 재요청
+  const data = await api.request(config);
 
   return Promise.resolve(data);
 }
 
-const resetToken = async function (refreshToken, accessToken){
+const resetToken = async function (refreshToken, accessToken, config){
+  // 기존 토큰 새로 업데이트
   localStorage.setItem('refreshToken', refreshToken);
   api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-  console.log('=========== retry ============');
+
+  // 재발급후 재요청 header의 액세스토큰 신규 토큰으로 변경
+  config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
 }
 
