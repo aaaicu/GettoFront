e99deafdf2dab2f705421289c519e320971b2fa4
aaaicu <aaaaicu@gmail.com>
2022-08-27 15:53:42 +0900

    feat: 로또 번호 표시 (axios 를 통해서 component 업데이트 정상 테스트)

--- src/component/lotto/Notice.js
+++ src/component/lotto/Notice.js
@@ -3,7 +3,6 @@ import './Notice.css';
 function Notice() {
   return (
     <div className="Notice">
-    공지 사항 공지 공지 사하아앙 공지사항
     </div>
   );
 }
--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
+import { api } from '../common/axios/customAxios';
 import Notice from '../component/lotto/Notice';
 import './Nav.css';
 
+
 function Nav() {
+  const [ luckyNumber, setLuckyNumber ] = useState([]); 
+
+  const data = {};
+  useEffect(()=> {
+    async function fetchData() {
+      const getLuckyNumber = await api.get('/api/last/lucky-number');
+      // console.log(getLuckyNumber.data);
+      setLuckyNumber(getLuckyNumber.data);
+    }
+
+    fetchData();
+  }, [])
+  
+
   return (
     <div className="Nav">
-      <Notice/>
-
+      <Notice />
       <div className='Menu'>
       버튼 | 영역 | 목록 | 짠
+      
+      </div>
+      <div>
+      {luckyNumber.round} 회차 ({luckyNumber.createdAt})
+      </div>
+      <div>
+      {luckyNumber.number1}, {luckyNumber.number2}, {luckyNumber.number3}, {luckyNumber.number4}, {luckyNumber.number5}, {luckyNumber.number6} + {luckyNumber.bonus}
       </div>
     </div>
   );
